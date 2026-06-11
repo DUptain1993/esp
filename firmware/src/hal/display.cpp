c1e47a98af80eb113790b2c65b1aee6389e88042
Daniel Uptain <daniel.r.uptain93@gmail.com>
2026-06-11 00:58:20 -0700

    Update display.cpp

--- firmware/src/hal/display.cpp
+++ firmware/src/hal/display.cpp
@@ -1,53 +1,48 @@
-#include "display.h"
+#include <Arduino.h>
+#include <TFT_eSPI.h>
+#include <lvgl.h>
 
-TFT_eSPI tft = TFT_eSPI();
+static TFT_eSPI tft = TFT_eSPI();
 
-/* LVGL display buffer */
+/* -------- BUFFERS -------- */
 static lv_disp_draw_buf_t draw_buf;
-static lv_color_t *buf1;
-static lv_color_t *buf2;
+static lv_color_t buf1[320 * 40];
+static lv_color_t buf2[320 * 40];
 
-void hal_display_init(void) {
-    // Explicitly set pin modes for control pins
-    pinMode(15, OUTPUT); // CS
-    digitalWrite(15, HIGH);
-    pinMode(2, OUTPUT);  // DC
-    digitalWrite(2, HIGH);
-    pinMode(27, OUTPUT); // Backlight
-    digitalWrite(27, HIGH);
-
-    tft.init();
-    tft.setRotation(0); // Portrait 320x480
-    tft.fillScreen(TFT_BLACK);
-    
-    // Initialize DMA
-    tft.initDMA();
-
-    // Allocate buffers in DMA-capable memory
-    buf1 = (lv_color_t *)heap_caps_malloc(320 * 40 * sizeof(lv_color_t), MALLOC_CAP_DMA);
-    buf2 = (lv_color_t *)heap_caps_malloc(320 * 40 * sizeof(lv_color_t), MALLOC_CAP_DMA);
-
-    lv_disp_draw_buf_init(&draw_buf, buf1, buf2, 320 * 40);
-
-    /* Initialize the display driver for LVGL */
-    static lv_disp_drv_t disp_drv;
-    lv_disp_drv_init(&disp_drv);
-    disp_drv.hor_res = 320;
-    disp_drv.ver_res = 480;
-    disp_drv.flush_cb = hal_display_flush;
-    disp_drv.draw_buf = &draw_buf;
-    lv_disp_drv_register(&disp_drv);
-}
-
-void hal_display_flush(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p) {
-    uint32_t w = (area->x2 - area->x1 + 1);
-    uint32_t h = (area->y2 - area->y1 + 1);
+/* -------- FLUSH -------- */
+void my_disp_flush(lv_disp_drv_t *disp,
+                   const lv_area_t *area,
+                   lv_color_t *color_p)
+{
+    uint32_t w = area->x2 - area->x1 + 1;
+    uint32_t h = area->y2 - area->y1 + 1;
 
     tft.startWrite();
     tft.setAddrWindow(area->x1, area->y1, w, h);
-    // pushColors with swap=true is standard for LVGL 8 on ESP32 + SPI Display
-    tft.pushColors((uint16_t *)color_p, w * h, true);
+    tft.pushColors((uint16_t *)&color_p->full, w * h, true);
     tft.endWrite();
 
-    lv_disp_flush_ready(disp_drv);
+    lv_disp_flush_ready(disp);
+}
+
+/* -------- INIT -------- */
+void hal_display_init()
+{
+    pinMode(27, OUTPUT);
+    digitalWrite(27, HIGH); // BACKLIGHT ON
+
+    tft.begin();
+    tft.setRotation(1);
+
+    lv_disp_draw_buf_init(&draw_buf, buf1, buf2, 320 * 40);
+
+    static lv_disp_drv_t disp_drv;
+    lv_disp_drv_init(&disp_drv);
+
+    disp_drv.hor_res = 320;
+    disp_drv.ver_res = 480;
+    disp_drv.flush_cb = my_disp_flush;
+    disp_drv.draw_buf = &draw_buf;
+
+    lv_disp_drv_register(&disp_drv);
 }
