ec746859441ceb7aea8da2cd51f0cbdcb40a061d
Daniel Uptain <daniel.r.uptain93@gmail.com>
2026-06-11 00:57:28 -0700

    Update lv_conf.h

--- firmware/src/lv_conf.h
+++ firmware/src/lv_conf.h
@@ -1,116 +1,26 @@
-/**
- * @file lv_conf.h
- * Configuration file for v8.3.x
- */
-
 #ifndef LV_CONF_H
 #define LV_CONF_H
 
-#include <stdint.h>
-
-/*====================
-   COLOR SETTINGS
- *====================*/
 #define LV_COLOR_DEPTH 16
 #define LV_COLOR_16_SWAP 0
-#define LV_COLOR_SCREEN_TRANSP 0
-#define LV_COLOR_CHROMA_KEY lv_color_hex(0x00ff00)
 
-/*=========================
-   MEMORY SETTINGS
- *=========================*/
 #define LV_MEM_CUSTOM 0
-#if LV_MEM_CUSTOM == 0
-    #define LV_MEM_SIZE (64U * 1024U)
-    #define LV_MEM_ADR 0
-    #define LV_MEM_POOL_INCLUDE <stdint.h>
-#else
-    #define LV_MEM_CUSTOM_INCLUDE <stdlib.h>
-    #define LV_MEM_CUSTOM_ALLOC   malloc
-    #define LV_MEM_CUSTOM_FREE    free
-    #define LV_MEM_CUSTOM_REALLOC realloc
-#endif
+#define LV_MEM_SIZE (64U * 1024U)
 
-/*====================
-   HAL SETTINGS
- *====================*/
 #define LV_TICK_CUSTOM 1
-#if LV_TICK_CUSTOM
-    #define LV_TICK_CUSTOM_INCLUDE "Arduino.h"
-    #define LV_TICK_CUSTOM_SYS_TIME_EXPR (millis())
-#endif
+#define LV_TICK_CUSTOM_INCLUDE "Arduino.h"
+#define LV_TICK_CUSTOM_SYS_TIME_EXPR (millis())
 
-#define LV_DPI_DEF 130
+#define LV_USE_LOG 0
 
-/*=======================
- * FEATURE CONFIGURATION
- *=======================*/
-#define LV_USE_ASSERT_NULL          1
-#define LV_USE_ASSERT_MEM           1
-#define LV_USE_ASSERT_STR           1
-#define LV_USE_ASSERT_OBJ           1
-#define LV_USE_ASSERT_STYLE         1
+#define LV_HOR_RES_MAX 320
+#define LV_VER_RES_MAX 480
 
-#define LV_USE_USER_DATA            1
-#define LV_FEAT_ANIMATION           1
+#define LV_USE_LABEL 1
+#define LV_USE_BTN 1
+#define LV_USE_BAR 1
 
-/*=====================
- *  COMPILER SETTINGS
- *====================*/
-#define LV_BIG_ENDIAN_SYSTEM 0
-#define LV_ATTRIBUTE_TICK_INC
-#define LV_ATTRIBUTE_TIMER_HANDLER
-#define LV_ATTRIBUTE_FLUSH_READY
-#define LV_ATTRIBUTE_MEM_ALIGN_SIZE 4
-#define LV_ATTRIBUTE_MEM_ALIGN
-#define LV_ATTRIBUTE_LARGE_CONST
-#define LV_EXPORT_CONST_INT(int_value) struct _dummy_##int_value
-
-/*===================
- *  GRAPHICS OBJECTS
- *===================*/
-#define LV_USE_ARC          1
-#define LV_USE_BAR          1
-#define LV_USE_BTN          1
-#define LV_USE_BTNMATRIX    1
-#define LV_USE_CANVAS       1
-#define LV_USE_CHECKBOX     1
-#define LV_USE_DROPDOWN     1
-#define LV_USE_IMG          1
-#define LV_USE_LABEL        1
-#define LV_LABEL_TEXT_SELECTION 1
-#define LV_LABEL_LONG_TXT_HINT 1
-#define LV_USE_LINE         1
-#define LV_USE_ROLLER       1
-#define LV_USE_SLIDER       1
-#define LV_USE_SWITCH       1
-#define LV_USE_TEXTAREA     1
-#define LV_TEXTAREA_DEF_PWD_SHOW_TIME 1500
-#define LV_USE_TABLE        1
-
-/*==================
- *  EXTRA COMPONENTS
- *==================*/
-#define LV_USE_FLEX         1
-#define LV_USE_GRID         1
-
-/*==================
- *  THEMES
- *==================*/
-#define LV_USE_THEME_DEFAULT 1
-#define LV_THEME_DEFAULT_DARK 1
-#define LV_THEME_DEFAULT_GROW 1
-#define LV_THEME_DEFAULT_TRANSITION_TIME 80
-
-/*==================
- *  FONT USAGE
- *==================*/
-#define LV_FONT_MONTSERRAT_12 1
 #define LV_FONT_MONTSERRAT_14 1
-#define LV_FONT_MONTSERRAT_16 1
-#define LV_FONT_MONTSERRAT_18 1
-#define LV_FONT_MONTSERRAT_20 1
-#define LV_FONT_MONTSERRAT_24 1
 #define LV_FONT_DEFAULT &lv_font_montserrat_14
 
-#endif /*LV_CONF_H*/
+#endif
